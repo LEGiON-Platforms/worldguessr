9f91954634de98585188cd6e61e76d00a468d190
Incog <incogcyberpunk@gmail.com>
2024-12-29 15:21:17 +0545

    Trying Deploy in Vercel 2, Disabled Eslint during Productin

--- next.config.js
+++ next.config.js
@@ -22,10 +22,7 @@ const forProduction = {
     assetPrefix: './',
 };
 
-
-/** @type {import('next').NextConfig} */
-const nextConfig =
-{
+const forDevelopment={
     webpack: (config, { webpack }) => {
         config.devServer = {
             hot: true, // Enable HMR
@@ -41,6 +38,27 @@ const nextConfig =
     output: process.env.ENV === "production" ? 'export' : undefined,
     assetPrefix: process.env.ENV === "production" ? './' : "",
 };
+
+
+/** @type {import('next').NextConfig} */
+const nextConfig ={
+    webpack: (config, { webpack }) => {
+        return config
+    },
+    sassOptions: {
+        includePaths: [path.join(__dirname, 'styles')],
+    },
+    images: {
+        unoptimized: true,
+    },
+    output: 'export',
+    assetPrefix: './',
+    eslint:{
+        ignoreDuringBuilds: true,
+    }
+};
+
+
 
 // module.exports = nextConfig;
 export default nextConfig;
