7e1cc3fedfb1f7a76919adcc0bbd6cbdc948742d
Incog <incogcyberpunk@gmail.com>
2025-01-01 15:04:41 +0545

    Need to fix issues due to the new Components added by Incog

--- api/googleAuth.js
+++ api/googleAuth.js
@@ -89,7 +89,6 @@ export default async function handler(req, res) {
     return res.status(405).json({ error: 'Method Not Allowed' });
   }
   
-  console.log(`Hello in the api routes , I am here`)
 
   const { code, secret } = req.body;
   if (!code) {
