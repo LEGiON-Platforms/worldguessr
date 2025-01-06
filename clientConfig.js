5f1928853dd5e4cc77993aaaf6b8a5f62d2c3faa
Incog <incogcyberpunk@gmail.com>
2025-01-06 22:43:43 +0545

    Changed the clienConfig.js , making sure it performs as expected
    Corrected the incorrect usage of the nullish operator, which would result in wrong paths to the websocket servers (eg: http://false/wg"

--- clientConfig.js
+++ clientConfig.js
@@ -6,9 +6,12 @@ export default function config() {
 
 
   return {
-    // apiUrl is localhost:3001 
-    "apiUrl": prefixHttp + ((process.env.NEXT_PUBLIC_ENV === "production" &&  process.env.NEXT_PUBLIC_API_URL) ?? "localhost:3001"),
-    "websocketUrl": prefixWs + ((process.env.NEXT_PUBLIC_ENV === "production" &&  process.env.NEXT_PUBLIC_WS_URL) ?? process.env.NEXT_PUBLIC_API_URL ?? "localhost:3002") + '/wg',
+    // apiUrl  in the original configuration is localhost:3001 
+    // "apiUrl": prefixHttp + ((process.env.NEXT_PUBLIC_ENV === "production" &&  process.env.NEXT_PUBLIC_API_URL) ?? "localhost:3001"),
+    "apiUrl": "/api",
+    "websocketUrl": prefixWs + (process.env.NEXT_PUBLIC_ENV === "production" ? process.env.NEXT_PUBLIC_WS_URL  : "localhost:3002") + '/wg',
+
+    // the nullish operator `??` is used to check if the left side is null or undefined, if it is, the right side is used
     "cronUrl": prefixHttp + (process.env.NEXT_PUBLIC_CRON_URL ?? "localhost:3003"),
 
   }
