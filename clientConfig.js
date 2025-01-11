7557807dea702658b49e72e51e6854b92055e846
Incog <incogcyberpunk@gmail.com>
2025-01-11 16:53:54 +0545

    Trying to make websockets work in production

--- clientConfig.js
+++ clientConfig.js
@@ -9,7 +9,7 @@ export default function config() {
     // "apiUrl": prefixHttp + ((process.env.NEXT_PUBLIC_ENV === "production" &&  process.env.NEXT_PUBLIC_API_URL) ?? "localhost:3001"),
     "apiUrl": prefixHttp + (process.env.NEXT_PUBLIC_ENV === "production" ?  process.env.NEXT_PUBLIC_API_URL:  "localhost:3000"),
 
-    "websocketUrl":prefixWs +  (process.env.NEXT_PUBLIC_ENV === "production" ?  process.env.NEXT_PUBLIC_WS_URL  : "localhost:3002"),
+    "websocketUrl":(process.env.NEXT_PUBLIC_ENV === "production" ?  (prefixHttp+process.env.NEXT_PUBLIC_WS_URL)  : "ws://localhost:3002"),
     // "websocketUrl":  process.env.NEXT_PUBLIC_ENV === "production" ? (prefixHttp+ process.env.NEXT_PUBLIC_WS_URL)  : "ws://localhost:3002",
 
     // the nullish operator `??` is used to check if the left side is null or undefined, if it is, the right side is used
